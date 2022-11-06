ab4a8c90ed4ac87f36d19291afa68ce97ad53b5b
rus <ruslan.cheremisin@gmail.com>
2022-11-06 13:32:26 +0200

    дз 1

---
+++ src/Data.java
@@ -0,0 +1,41 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Data {
+    private final String login;
+    private final String password;
+    private final String confirmPassword;
+
+
+    public Data(String login, String password, String confirmPassword) throws WrongLoginException, WrongPasswordException{
+
+        Pattern pt = Pattern.compile("\\w{1,20}");
+        Matcher mt=pt.matcher(login);
+
+        if(!mt.matches()){
+            throw new WrongLoginException("Wrong Login Exception");
+        }else {
+            this.login = login;
+        }
+        mt=pt.matcher(password);
+        if(!mt.matches()){
+            throw new WrongPasswordException("Wrong Password Exception");
+        }else {
+            this.password = password;}
+        mt=pt.matcher(confirmPassword);
+        if(!password.equals(confirmPassword)){
+            throw new WrongPasswordException("Wrong Password Exception: Confirming password doesn't match");
+        }else {
+            this.confirmPassword = confirmPassword;}
+
+    }
+
+    public void checkPassword(String text){
+
+    }
+
+    public void getLoginPasswordInfo() {
+        System.out.println(login + "\n"+password);
+    }
+}
+
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
-    public static void main(String[] args) throws WrongLoginException {
-
+    public static void main(String[] args) throws WrongLoginException, WrongPasswordException {
+        Data data = new Data("1asd234567890","123_","123_");
+        data.getLoginPasswordInfo();
     }
 }
